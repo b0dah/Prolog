88ff007312a92e0633fc28bf2b79c275d053c542
b0dah <ries8898@gmail.com>
2019-12-03 22:39:27 +0300

    count occurances works fine

--- ForthIndividual(Files).pl
+++ ForthIndividual(Files).pl
@@ -20,20 +20,37 @@ writefacts:-
 %	 и добавлением в конце каждой строки тех слов из этой строки, 
 %	которые встречаются в файле ровно один раз в том порядке, в котором они встретились в строке.
 
-%file_to_list
-
-
-
-
-
-run :- see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readln(X), seen, write(X).
-
-fileToList(L) :- seen, see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readfile([],[],L), seen.
+%divide into a list of strings
+fileToListOfStrings(L) :- seen, see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readStringsFromFile([],[],L), seen.
 	
-readfile(T,R,L) :- get0(X), 
+readStringsFromFile(T,R,L) :- get0(X), 
 (X<0, (T=[], L=R; name(T1, T), append(R, [T1], L)), ! ;
 
  (X=10; X=13),
- (T=[], readfile(T, R, L); name(T1, T),
- 	append(R, [T1], R1), readfile([], R1, L )), !;
- 	X>0, append(T, [X], T1), readfile(T1, R, L), !).
+ (T=[], readStringsFromFile(T, R, L); name(T1, T),
+ 	append(R, [T1], R1), readStringsFromFile([], R1, L )), !;
+ 	X>0, append(T, [X], T1), readStringsFromFile(T1, R, L), !).
+
+%divide into a list of words
+fileToListOfWords(L) :- seen, see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readWords([],[],L), seen.
+	
+readWords(T,R,L) :- get0(X), 
+(X<0, (T=[], L=R; name(T1, T), append(R, [T1], L)), ! ;
+
+(X=32; X=10; X=13),
+ (T=[], readWords(T, R, L); name(T1, T),
+ 	append(R, [T1], R1), readWords([], R1, L )), !;
+ 	X>0, append(T, [X], T1), readWords(T1, R, L), !).
+
+
+%solve(Res) :- fileToListOfWords(L), deleteQuadruples(L, Res).
+%
+%deleteQuadruples([H|R], R) :- count_occurrences(L, ).
+
+%count_occurrences(List, Occ):-
+%	findall([X,L], (bagof(true,member(X,List),Xs), length(Xs,L)), Occ).
+	
+count_occurrences(List, Occ, Count):-
+bagof(true,member(Occ,List),Xs), length(Xs,L), Count is L.
+	
+	
