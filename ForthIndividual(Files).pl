2494c4f8928dc2c21de8d508dbe7458adbe9f396
b0dah <ries8898@gmail.com>
2019-12-04 18:10:20 +0300

    first part works

--- ForthIndividual(Files).pl
+++ ForthIndividual(Files).pl
@@ -32,8 +32,7 @@ readStringsFromFile(T,R,L) :- get0(X),
  	X>0, append(T, [X], T1), readStringsFromFile(T1, R, L), !).
 
 %divide into a list of words
-fileToListOfWords(L) :- seen, see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readWords([],[],L), seen.
-	
+fileToListOfWords(L) :- seen, see('/Users/bodah/dev/prolog/TextFiles/input.txt'), readWords([],[],L), seen.	
 readWords(T,R,L) :- get0(X), 
 (X<0, (T=[], L=R; name(T1, T), append(R, [T1], L)), ! ;
 
@@ -42,15 +41,29 @@ readWords(T,R,L) :- get0(X),
  	append(R, [T1], R1), readWords([], R1, L )), !;
  	X>0, append(T, [X], T1), readWords(T1, R, L), !).
 
-
-%solve(Res) :- fileToListOfWords(L), deleteQuadruples(L, Res).
-%
-%deleteQuadruples([H|R], R) :- count_occurrences(L, ).
-
-%count_occurrences(List, Occ):-
-%	findall([X,L], (bagof(true,member(X,List),Xs), length(Xs,L)), Occ).
-	
-count_occurrences(List, Occ, Count):-
+% count of element occurences in the list
+count_occurrences(Occ, List, Count):-
 bagof(true,member(Occ,List),Xs), length(Xs,L), Count is L.
-	
-	
+
+% delete 4 times occurrences
+delete4x([], Res, Res) :- !.
+delete4x([H|T], Res, Ans) :- count_occurrences(H, T, C), C>2, delete([H|T], H, Res1), !, delete4x(Res1, Res, Ans).
+delete4x([H|T], Res, Ans) :- append(Res,[H],Res1), delete4x(T, Res1, Ans). 
+
+% /////////// MAIN //////////////
+solve(Res) :- fileToListOfWords(L), delete4x(L, [], Res).
+
+
+
+%/////////////////// LEGACY ////////////////////////
+%
+%ind4([],R,R):-!.	 % WORKS WITH THE SOURCE LIST IN TWO LAST PARAMETERS
+%ind4([H|T],Res,Ans):-count_occurrences(H,T,Count),Count > 2,!,include(is_not(H),Res, IntermRes),ind4(T,IntermRes,Ans).	
+%ind4([H|T],Res,Ans):-ind4(T,Res,Ans).
+%
+%is_not(X, Y) :- X \= Y.
+
+%
+%deleteQuadruples([], []) :- !.
+%deleteQuadruples([H|R], ResList) :- count_occurrences(H, [H|R], C), C > 3, !, deleteQuadruples(R, ResList).
+%deleteQuadruples([H|R], [H|ResList]) :- deleteQuadruples(R, ResList).
